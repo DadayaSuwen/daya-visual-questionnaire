3c595b4c5c6b968b9a190b72fa3e2f894833b670
housuwen <housuwen@sugo.io>
2023-05-25 19:32:39 +0800

    fix: 项目重构

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
+import ListPage from './pages/list-page'
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>问卷列表</h1>
+      <ListPage />
     </div>
   )
 }
-
-export default App
---
+++ src/pages/list-page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function ListPage() {
+  return <div>list-page</div>
+}
+export default ListPage
